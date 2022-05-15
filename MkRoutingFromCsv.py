ff8d71010dfb811183ad490439a65dda45850327
Leo <lars.erik.rojeras@skoview.se>
2022-05-15 19:54:02 +0200

    Implemented BsJson class for json-generation

--- MkRoutingFromCsv.py
+++ MkRoutingFromCsv.py
@@ -33,12 +33,166 @@ def get_header():
         "utforare": "Region Stockholm - Forvaltningsobjekt Informationsinfrastruktur",
         "genomforandeTidpunkt": now,
     }
+##################################################################################################
+# """
+class BsJson:
+    def __init__(self, plattform, executor="Region Stockholm - Forvaltningsobjekt Informationsinfrastruktur"):
+        self.plattform = plattform
+        self.executor = executor
+        self.include = None
+        self.exclude = None
+
+    def add_section(self, type, section):
+
+        if type == "include":
+            self.include = section
+        elif type == "exclude":
+            self.exclude = section
+        else:
+            printerr(f"add_section called with unknown type: {type}")
+            exit(1)
+
+    def get_headerx(self):
+        now = datetime.now().strftime("%Y-%m-%dT%H:%M:%S+0100")
+
+        return {
+            "plattform": self.plattform,
+            "formatVersion": "1.0",
+            "version": "1",
+            "bestallningsTidpunkt": now,
+            "utforare": self.executor,
+            "genomforandeTidpunkt": now,
+        }
+
+    def print_json(self):
+
+        content = self.get_headerx()
+        if self.include:
+            content["inkludera"] = BsJsonSection.get_json(self.include)
+        if self.exclude:
+            content["exkludera"] = BsJsonSection.get_json(self.exclude)
+
+        printerr(f"Generating UPDATE file")
+        print(json.dumps(content, ensure_ascii=False, indent=4))
+
+
+class BsJsonSection:
+    def __init__(self):
+        self.logicalAddresses = []
+        self.components = []
+        self.contracts = []
+        self.routings = []
+        self.authorities = []
+
+    def add_logicalAddress(self,
+                           id,
+                           description):
+
+        logicalAddress = {
+            "hsaId": id,
+            "beskrivning": description
+        }
+
+        if logicalAddress not in self.logicalAddresses:
+            self.logicalAddresses.append(logicalAddress)
+
+    def add_component(self, id, description):
+
+        component = {
+            "hsaId": id,
+            "beskrivning": description
+        }
+
+        if component not in self.components:
+            self.components.append(component)
+
+    def add_contract(self,
+                     namespace,
+                     description):
+
+        ix = namespace.rfind(":") + 1
+        majorStr = namespace[ix:]
+        major = int(majorStr)
+
+        contract = {
+            "namnrymd": namespace,
+            "beskrivning": description,
+            "majorVersion": major
+        }
+
+        if contract not in self.contracts:
+            self.contracts.append(contract)
+
+    def add_routing(self,
+                    component,
+                    address,
+                    logicalAddress,
+                    namespace,
+                    rivtaProfile="RIVTABP21"):
+
+        routing = {
+            "tjanstekomponent": component,
+            "adress": address,
+            "logiskAdress": logicalAddress,
+            "tjanstekontrakt": namespace,
+            "rivtaprofil": rivtaProfile
+        }
+        if routing not in self.routings:
+            self.routings.append(routing)
+
+    def get_json(self):
+
+        return {
+            "tjanstekontrakt": self.contracts,
+            "tjanstekomponenter": self.components,
+            "logiskadresser": self.logicalAddresses,
+            "anropsbehorigheter": self.authorities,
+            "vagval": self.routings,
+        }
+
+    # """
+
 
 
 ##################################################################################################
 
-
 def create_json_file() -> None:
+    """Will create json file"""
+
+    include_section = BsJsonSection()
+
+    for contract in get_json_contracts():
+        include_section.add_contract(contract["namnrymd"], contract["beskrivning"])
+
+    with open(CSV_FILE) as csv_file:
+
+        csv_reader = csv.reader(csv_file, delimiter=';')
+
+        for row in csv_reader:
+            la_hsaid = row[0].strip()  # Pick up from file
+            la_description = row[1].strip()  # Pick up from file
+
+            include_section.add_logicalAddress(la_hsaid, la_description)
+            include_section.add_routing(PRODUCER_HSA_ID,
+                                        PRODUCER_URL,
+                                        la_hsaid,
+                                        SERVICE_CONTRACT_NAMESPACE["ProcessRequest"]
+                                        )
+            include_section.add_routing(PRODUCER_HSA_ID,
+                                        PRODUCER_URL,
+                                        la_hsaid,
+                                        SERVICE_CONTRACT_NAMESPACE["ProcessRequestConfirmation"]
+                                        )
+            include_section.add_routing(PRODUCER_HSA_ID,
+                                        PRODUCER_URL,
+                                        la_hsaid,
+                                        SERVICE_CONTRACT_NAMESPACE["ProcessRequestOutcome"]
+                                        )
+    content = BsJson("TEST-TP")
+    content.add_section("include", include_section)
+    content.print_json()
+
+def xxxcreate_json_file() -> None:
     """Will create json file"""
 
     include = {}
@@ -95,34 +249,7 @@ def create_json_file() -> None:
     print(json.dumps(content, ensure_ascii=False, indent=4))
 
 
-##################################################################################################
-"""
-class bs_json:
-    def __int__(self, plattform, executor="Region Stockholm - Forvaltningsobjekt Informationsinfrastruktur"):
-        self.plattform = plattform
-        self.executor = executor
-        self.include = {}
-        self.exclude = {}
-        self.include_routing = []
-        self.exclude_routing = []
-        self.include_logicaladdresses = []
-        self.exclude_logicaladdresses = []
 
-
-
-    def get_header(self):
-        now = datetime.now().strftime("%Y-%m-%dT%H:%M:%S+0100")
-
-        return {
-            "plattform": self.plattform,
-            "formatVersion": "1.0",
-            "version": "1",
-            "bestallningsTidpunkt": now,
-            "utforare": self.executor,
-            "genomforandeTidpunkt": now,
-        }
-
-"""
 ##################################################################################################
 
 
