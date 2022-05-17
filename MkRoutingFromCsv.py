e04676bc6dc464ab17cc828ac3210adf3df750a2
Leo <lars.erik.rojeras@skoview.se>
2022-05-17 08:15:10 +0200

    MkRoutingFromCsv now fetch logical address descriptions from TAK-api i it is run with the -r flag.

--- MkRoutingFromCsv.py
+++ MkRoutingFromCsv.py
@@ -1,6 +1,7 @@
 import sys
 import argparse
 import csv
+import requests
 from BsJson import BsJson, BsJsonSection
 
 ##################################################################################################
@@ -17,15 +18,37 @@ def printerr(text):
     print(text, file=sys.stderr)
 
 
+def get_la_from_takaip():
+
+    # Ensure order to get LA from TP with highest rank
+    ntjp_prod_id = 4
+    rtp_prod_id = 5
+    ntjp_qa_id = 6
+    rtp_qa_id = 7
+    tp_ids = [ntjp_prod_id, rtp_prod_id, ntjp_qa_id, rtp_qa_id]
+
+    logical_addess_descriptions = {}
+
+    for tp_id in tp_ids:
+        response = requests.get(f"http://api.ntjp.se/coop/api/v1/logicalAddresss?connectionPointId={tp_id}")
+        for la in response.json():
+            la_id = la["logicalAddress"]
+            la_desc = la["description"]
+            if la_id not in logical_addess_descriptions:
+                logical_addess_descriptions[la_id] = la_desc
+
+    return logical_addess_descriptions
 ##################################################################################################
 #                                 Main Program
 ##################################################################################################
 # Parse arguments
 parser = argparse.ArgumentParser()
-# parser.add_argument("filename", nargs=1, type=argparse.FileType('r'))
+parser.add_argument("-r", "--replace", action='store_true',
+                    help="replace logical address descriptions from TAK-api")
 parser.add_argument("filename", nargs=1)
 args = parser.parse_args()
 
+REPLACE_LA_DESCRIPTION = args.replace
 CSV_FILE = args.filename[0]
 
 ##################################################################################################
@@ -55,6 +78,11 @@ SERVICE_CONTRACTS = [
 ##################################################################################################
 # And the action
 
+logical_address_descriptions = {}
+
+if REPLACE_LA_DESCRIPTION:
+    logical_address_descriptions = get_la_from_takaip()
+
 include_section = BsJsonSection()
 
 for contract in SERVICE_CONTRACTS:
@@ -66,6 +94,9 @@ with open(CSV_FILE) as csv_file:
     for row in csv_reader:
         la_hsaid = row[0].strip()  # Pick up from file
         la_description = row[1].strip()  # Pick up from file
+
+        if la_hsaid in logical_address_descriptions:
+            la_description = logical_address_descriptions[la_hsaid]
 
         include_section.add_logicalAddress(la_hsaid, la_description)
 
