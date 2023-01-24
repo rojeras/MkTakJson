e68196e063badc515017258aea9f9b52b9dc6eff
Leo <lars.erik.rojeras@skoview.se>
2023-01-24 08:54:10 +0100

    Cleanup

--- archive/takapiProductions.sh
+++ archive/takapiProductions.sh
@@ -1,5 +1,7 @@
 #!/bin/sh 
 
+# This script will fetch production (vägval) information from the TAK-api and print.
+# Dumped to a file it is used as input to the RequestReReoute.py script.
 
 curl -X GET "http://api.ntjp.se/coop/api/v1/serviceProductions?include=connectionPoint,logicalAddress,serviceContract,serviceProducer,physicalAddress" -H "accept: application/json" 
 
